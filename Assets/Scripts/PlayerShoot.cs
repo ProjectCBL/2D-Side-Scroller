5971abc1c63d382b632152541a19c6e0f606f0cc
ProjectCBL <projectxcbl@gmail.com>
2021-10-01 13:47:22 -0400

    Added a function for spawning bullets

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -7,21 +7,41 @@ public class PlayerShoot : MonoBehaviour
 {
 
     public GameObject bullet;
+    public GameObject muzzleFlare;
     public GameObject bulletSpawn;
     public GameObject bulletContainer;
+    public PlayerMovement movementScript;
 
     [SerializeField] private bool shootPressed = false;
 
-    // Start is called before the first frame update
-    void Start()
+    // Called once the game object is enabled
+    void Awake()
     {
-        
+        bulletContainer = GameObject.Find("Bullet Container");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Shoot();
+    }
+
+    /* ==================== Shooting Bullets ==================== */
+
+    private void Shoot()
+    {
+        if (shootPressed)
+        {
+            OrientBulletDirection(SpawnBullet());
+            shootPressed = !shootPressed;
+        }
+    }
+
+    private void OrientBulletDirection(GameObject bullet)
+    {
+        Vector3 newLocalScale = bullet.transform.localScale;
+        newLocalScale.x *= (movementScript.playerFacingRight) ? 1 : -1;
+        bullet.transform.localScale = newLocalScale;
     }
 
     private GameObject SpawnBullet(){
@@ -32,6 +52,8 @@ public class PlayerShoot : MonoBehaviour
             bulletContainer.transform
         );
     }
+
+    /* ==================== Shoot Button Input ==================== */
 
     public void OnShoot(InputAction.CallbackContext ctx){
         shootPressed = ctx.performed ? true : false;
