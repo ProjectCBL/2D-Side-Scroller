3d4ff9f26aee9d67c2779a4f0745aa2ae745111c
ProjectCBL <projectxcbl@gmail.com>
2021-10-25 14:52:56 -0400

    Adjusted script to perform multiple behaviours and cycle between points

--- Assets/Scripts/Map/MovePlatform.cs
+++ Assets/Scripts/Map/MovePlatform.cs
@@ -5,47 +5,122 @@ using UnityEngine;
 public class MovePlatform : MonoBehaviour
 {
 
-    [Range(0, 0.5f)] public float platformSpeed = 0.25f;
+    private enum BehaviourType
+    {
+        MOVEBACKANDFORTH,
+        MOVETOONEANDPAUSE,
+        MoveTOTWOANDPAUSE,
+        PAUSE
+    } 
+
+    [Range(0, 2.5f)] public float platformSpeed = 0.25f;
 
     private Vector2 targetPoint;
-    [SerializeField] private Vector2 upperPoint;
-    [SerializeField] private Vector2 lowerPoint;
+    [SerializeField] private Transform point1;
+    [SerializeField] private Transform point2;
     [SerializeField] private float pointDifference = 0.5f;
+    [SerializeField] private BehaviourType behaviourSwitch = BehaviourType.MOVEBACKANDFORTH;
  
     // Start is called before the first frame update
     void Start()
     {
-        SetTargetPoints();
-        targetPoint = upperPoint;
+        PickBehaviour();
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MoveBackAndForth()
     {
-        SwithDirection();
-        MoveToward();
+
+        targetPoint = point1.localPosition;
+
+        while (true)
+        {  
+            SwitchDirection();
+            MoveToward();
+            yield return null;
+        }   
+    }
+
+    private IEnumerator MoveToOneAndPause()
+    {
+        targetPoint = point1.localPosition;
+
+        while (true)
+        {
+            MoveToward();
+
+            if (CheckIfPlatformIsAt(point1))
+                break;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveToTwoAndPause()
+    {
+        targetPoint = point2.localPosition;
+
+        while (true)
+        {
+            MoveToward();
+
+            if (CheckIfPlatformIsAt(point2))
+                break;
+
+            yield return null;
+        }
     }
 
     private void MoveToward()
     {
-        transform.position = Vector2.MoveTowards(
-            transform.position,
+        transform.localPosition = Vector2.MoveTowards(
+            transform.localPosition,
             targetPoint,
             platformSpeed * Time.deltaTime);
     }
 
-    private void SwithDirection()
+    private void SwitchDirection()
     {
-        if (transform.position.y >= upperPoint.y)
-            targetPoint = lowerPoint;
-        else if (transform.position.y <= lowerPoint.y)
-            targetPoint = upperPoint;
+        if (CheckIfPlatformIsAt(point1))
+            targetPoint = point2.transform.localPosition;
+        else if (CheckIfPlatformIsAt(point2))
+            targetPoint = point1.transform.localPosition;
     }
 
-    private void SetTargetPoints()
+    private bool CheckIfPlatformIsAt(Transform point)
     {
-        float y = transform.position.y;
-        upperPoint = new Vector2(transform.position.x, y + pointDifference);
-        lowerPoint = new Vector2(transform.position.x, y - pointDifference);
+        return (transform.localPosition == point.localPosition);
+    }
+
+    private void ClampPosition()
+    {
+        float x = Mathf.Clamp(
+            transform.localPosition.x, 
+            point1.localPosition.x, 
+            point2.localPosition.x);
+        float y = Mathf.Clamp(
+            transform.localPosition.y, 
+            point1.localPosition.y, 
+            point2.localPosition.y);
+        transform.localPosition = new Vector2(x, y);
+    }
+
+    private void PickBehaviour()
+    {
+        switch (behaviourSwitch)
+        {
+            case BehaviourType.MOVETOONEANDPAUSE:
+                StartCoroutine(MoveToOneAndPause());
+                break;
+            case BehaviourType.MoveTOTWOANDPAUSE:
+                StartCoroutine(MoveToTwoAndPause());
+                break;
+            case BehaviourType.MOVEBACKANDFORTH:
+                StartCoroutine(MoveBackAndForth());
+                break;
+            case BehaviourType.PAUSE:
+                break;
+            default:
+                break;
+        }
     }
 }
