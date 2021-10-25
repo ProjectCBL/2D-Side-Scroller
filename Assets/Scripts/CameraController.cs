c5645345b09ab845bfb326299d317a0185b17827
ProjectCBL <projectxcbl@gmail.com>
2021-10-25 14:30:01 -0400

    Created a camera that follows the player on the x-axis

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject player, anchorPoint1, anchorPoint2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        AdjustXAxis();
+    }
+
+    private void AdjustXAxis()
+    {
+        float x = Mathf.Clamp(
+            player.transform.position.x, 
+            anchorPoint1.transform.position.x,
+            anchorPoint2.transform.position.x);
+        transform.position = new Vector3(
+            x,
+            transform.position.y,
+            -10);
+    }
+
+}
